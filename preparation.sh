7cde74145a1e50a0c173101ea6590991317a3815
Shintaro Shiba <shiba.shintaro@gmail.com>
2017-01-07 16:00:10 +0900

    separate pip-install of dependencies from preparation.sh

--- preparation.sh
+++ preparation.sh
@@ -1,4 +1,4 @@
-### go installation
+### go lang installation
 wget https://storage.googleapis.com/golang/go1.7.3.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.7.3.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
@@ -24,8 +24,7 @@ skicka download java_song data/
 
 ### install dependencies for wave resample
 sudo pip install --upgrade pip
-pip install --user argparse
-pip install --user tqdm
+pip install --user -r requirements.txt
 
 
 ### resampling data
@@ -38,7 +37,5 @@ git clone https://github.com/ibab/tensorflow-wavenet.git
 cd tensorflow-wavenet
 pip install --user -r requirements.txt
 cd ..
-
-
 
 
